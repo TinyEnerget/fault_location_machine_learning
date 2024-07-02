0018754a4d691cb1cfa29ba82dc69321d1d94d9e
Vlad Titov <WagnerMean@yandex.ru>
2024-07-02 12:06:41 +0300

    Update

--- alg_repository/data_importer.py
+++ alg_repository/data_importer.py
@@ -5,11 +5,19 @@ import pandas as pd
 class DataImporter:
 
     def __init__(self,sep = ';', input_file_path = None):
-        self.file_path = None
-        self.file_type = None
-        self.data = None
+        self.file_path = str
+        self.file_type = str
+        self.data = pd.DataFrame()
         self.sep = sep
         self.input_file_path = input_file_path
+        self.file_type_mapping = {
+            'csv': pd.read_csv,
+            'tsv': pd.read_csv,
+            'xlsx': pd.read_excel,
+            'xls': pd.read_excel,
+            'xlsm': pd.read_excel,
+            'xlsb': pd.read_excel
+            }
     
 
     def select_file(self):
@@ -31,23 +39,36 @@ class DataImporter:
         self.file_type = self.file_path.split('.')[-1]
         return self
     
+        
+
     def import_data(self) -> pd.DataFrame:
-        if self.file_type == 'csv' or self.file_type == 'CSV':
-            self.data = pd.read_csv(self.file_path, sep = self.sep)
-        elif self.file_type == 'tsv':
-            self.data = pd.read_csv(self.file_path, sep = self.sep)
-        elif self.file_type == 'xlsx':
-            self.data = pd.read_excel(self.file_path)
-        elif self.file_type == 'xls':
-            self.data = pd.read_excel(self.file_path)
-        elif self.file_type == 'xlsm':
-            self.data = pd.read_excel(self.file_path)
-        elif self.file_type == 'xlsb':
-            self.data = pd.read_excel(self.file_path)
+        file_path = self.file_path
+        file_type_mapping = self.file_type_mapping
+        file_type = str(self.file_type)
+        if file_type.lower() in file_type_mapping:
+            self.data = file_type_mapping[file_type](file_path, sep=self.sep)
         else:
             print('File type not supported')
-        
         return self.data
+
+    #def import_data(self) -> pd.DataFrame:
+    #    file_path = self.file_path
+    #    if self.file_type == 'csv' or self.file_type == 'CSV':
+    #        self.data = pd.read_csv(file_path, sep = self.sep) # type: ignore
+    #    elif self.file_type == 'tsv':
+    #        self.data = pd.read_csv(file_path, sep = self.sep)
+    #    elif self.file_type == 'xlsx':
+    #        self.data = pd.read_excel(file_path)
+    #    elif self.file_type == 'xls':
+    #        self.data = pd.read_excel(file_path)
+    #    elif self.file_type == 'xlsm':
+    #        self.data = pd.read_excel(file_path)
+    #    elif self.file_type == 'xlsb':
+    #        self.data = pd.read_excel(file_path)
+    #    else:
+    #        print('File type not supported')
+    #    
+    #    return self.data
     
     def main_process(self):
         self = DataImporter.select_file(self)
