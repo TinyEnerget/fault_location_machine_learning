dc48850d6e9b2f13bdf8c5ad17ab817466701e5a
Vlad Titov <WagnerMean@yandex.ru>
2024-08-22 16:50:59 +0300

    update

--- alg_repository/PMU_relative_unit.py
+++ alg_repository/PMU_relative_unit.py
@@ -2,90 +2,44 @@ import numpy as np
 import pandas as pd
 
 class PMU_relative_units:
-    """
-    Calculates the symmetrical components (positive, negative, and zero sequence) from the input phase magnitudes and angles.
-    
-    The `vectors_calculation` method constructs the phase vectors from the input magnitudes and angles, and then uses the symmetrical component transformation matrix to calculate the symmetrical components.
-    
-    Args:
-        magnitude_A (pd.DataFrame): A DataFrame containing the magnitudes of phase A.
-        magnitude_B (pd.DataFrame): A DataFrame containing the magnitudes of phase B.
-        magnitude_C (pd.DataFrame): A DataFrame containing the magnitudes of phase C.
-        angle_A (pd.DataFrame): A DataFrame containing the angles of phase A.
-        angle_B (pd.DataFrame): A DataFrame containing the angles of phase B.
-        angle_C (pd.DataFrame): A DataFrame containing the angles of phase C.
-    
-    Returns:
-        None
-    """
         
     def __init__(self,
                  magnitude_A: pd.DataFrame,
                  magnitude_B: pd.DataFrame,
                  magnitude_C: pd.DataFrame,
-                 angle_A: pd.DataFrame,
-                 angle_B: pd.DataFrame,
-                 angle_C: pd.DataFrame
+                 measurment_type: str
                  ):
-        """
-        Initializes the PMU_symmetrical_components class with the necessary input data.
-        
-        Args:
-            magnitude_A (pd.DataFrame): A DataFrame containing the magnitudes of phase A.
-            magnitude_B (pd.DataFrame): A DataFrame containing the magnitudes of phase B.
-            magnitude_C (pd.DataFrame): A DataFrame containing the magnitudes of phase C.
-            angle_A (pd.DataFrame): A DataFrame containing the angles of phase A.
-            angle_B (pd.DataFrame): A DataFrame containing the angles of phase B.
-            angle_C (pd.DataFrame): A DataFrame containing the angles of phase C.
-        
-        Attributes:
-            sym_factor (np.ndarray): The symmetrical component transformation matrix.
-            zero_seq (pd.DataFrame): A DataFrame to store the zero sequence components.
-            zero_seq_angle (pd.DataFrame): A DataFrame to store the angles of the zero sequence components.
-            negative_seq (pd.DataFrame): A DataFrame to store the negative sequence components.
-            negative_seq_angle (pd.DataFrame): A DataFrame to store the angles of the negative sequence components.
-            positive_seq (pd.DataFrame): A DataFrame to store the positive sequence components.
-            positive_seq_angle (pd.DataFrame): A DataFrame to store the angles of the positive sequence components.
-            magnitude_A (pd.DataFrame): The input magnitudes of phase A.
-            magnitude_B (pd.DataFrame): The input magnitudes of phase B.
-            magnitude_C (pd.DataFrame): The input magnitudes of phase C.
-            angle_A (pd.DataFrame): The input angles of phase A.
-            angle_B (pd.DataFrame): The input angles of phase B.
-            angle_C (pd.DataFrame): The input angles of phase C.
-        """
                 
-        self.sym_factor = self._symmetrical_factor()
-        self.zero_seq = pd.DataFrame()
-        self.zero_seq_angle = pd.DataFrame()
-        self.negative_seq = pd.DataFrame()
-        self.negative_seq_angle = pd.DataFrame()
-        self.positive_seq = pd.DataFrame()
-        self.positive_seq_angle = pd.DataFrame()
         self.magnitude_A = magnitude_A
         self.magnitude_B = magnitude_B
         self.magnitude_C = magnitude_C
-        self.angle_A = angle_A
-        self.angle_B = angle_B
-        self.angle_C = angle_C
+        self.measurment_type = measurment_type
+        self.MeanNominalPhaseVoltages = [
+            3.15,
+            6.3,
+            10.5,
+            37,
+            115,
+            154,
+            230,
+            340,
+            400,
+            515,
+            750,
+            1150
+        ] / np.sqrt(3)
 
+    def measurment(self):
 
-    def _symmetrical_factor(self) -> np.ndarray:
-        """
-        Calculates the symmetrical component transformation matrix.
-        
-        The symmetrical component transformation matrix is used to transform a three-phase
-        system into its symmetrical components (positive, negative, and zero sequence).
-        This method returns the 3x3 transformation matrix.
-        """
-                
-        alfa = np.exp(1j * 2 * np.pi/3)
-        sym_factor = np.array([
-            [1, 1, 1],
-            [1, alfa**2, alfa],
-            [1, alfa, alfa**2]
-        ])
-        return sym_factor
-    
+        if self.measurment_type == 'voltage':
+            return self.vectors_voltage_calculation()
+
+        elif self.measurment_type == 'current':
+            return self.vectors_current_calculation()
+
+        else:
+            raise Exception('Measurement type not recognized')
+
     def _vector_construction(self, exp, idx) -> np.ndarray:
         """
         Constructs a vector of complex phase values from the input magnitudes and angles.
@@ -99,55 +53,38 @@ class PMU_relative_units:
         """
                 
         phase_vectors = np.array([
-        self.magnitude_A[exp][idx]*np.exp(1j*self.angle_A[exp][idx]),
-        self.magnitude_B[exp][idx]*np.exp(1j*self.angle_B[exp][idx]),
-        self.magnitude_C[exp][idx]*np.exp(1j*self.angle_C[exp][idx])
-        ])
+            self.magnitude_A[exp][idx],
+            self.magnitude_B[exp][idx],
+            self.magnitude_C[exp][idx]
+            ])
         return phase_vectors
 
-    def vectors_calculation(self) -> dict:
-        """
-        Calculates the symmetrical components (positive, negative, and zero sequence) from the input phase magnitudes and angles.
-        
-        This method iterates through the input data, constructs the complex phase vectors, and then applies the symmetrical component transformation matrix to obtain the symmetrical components. The results are stored in the corresponding DataFrame attributes.
-        
-        Returns:
-            dict: A dictionary containing the calculated symmetrical components, with keys for zero sequence, zero sequence angle, negative sequence, negative sequence angle, positive sequence, and positive sequence angle.
-        """
-                
-                
-        sym_factor = self.sym_factor
-        for exp in self.magnitude_A.columns.values:
-            zero_seq_tmp = []
-            zero_seq_angle_tmp = []
-            negative_seq_tmp = []
-            negative_seq_angle_tmp = []
-            positive_seq_tmp = []
-            positive_seq_angle_tmp = []
+    def SteadystateNormalCalculation(self):
+        idx = 0
+        tmp_list = []
+        idx_list = []
+        cur_norm = []
+        for item in dif_cur:
+            if np.abs(np.round(item, decimals=0)) <= 0 and idx != 0:
+                tmp_list.append(idx)
+                cur_norm.append(tmp_cur[idx-1])
+                idx_list.append(idx-1)
 
-            for idx in range(len(self.magnitude_A[exp])):
-                phase_vectors = self._vector_construction(exp, idx)
-                sym_comp = 1/3 * np.transpose(phase_vectors.transpose().dot(sym_factor))
-                zero_seq_tmp.append([np.abs(sym_comp[0])]) 
-                zero_seq_angle_tmp.append([np.angle(sym_comp[0])])
-                negative_seq_tmp.append([np.abs(sym_comp[1])])
-                negative_seq_angle_tmp.append([np.angle(sym_comp[1])])
-                positive_seq_tmp.append([np.abs(sym_comp[2])])
-                positive_seq_angle_tmp.append([np.angle(sym_comp[2])])
-
-            self.zero_seq[exp] = pd.DataFrame(zero_seq_tmp)
-            self.zero_seq_angle[exp] = pd.DataFrame(zero_seq_angle_tmp)
-            self.negative_seq[exp] = pd.DataFrame(negative_seq_tmp)
-            self.negative_seq_angle[exp] = pd.DataFrame(negative_seq_angle_tmp)
-            self.positive_seq[exp] = pd.DataFrame(positive_seq_tmp)
-            self.positive_seq_angle[exp] = pd.DataFrame(positive_seq_angle_tmp)
+            if tmp_list != []:
+                if np.abs(tmp_list[-1]-idx) == 1:
+                    print(tmp_list)
+                    break
+            idx += 1
         
-        dict_seq = {
-            'zero_seq': self.zero_seq,
-            'zero_seq_angle': self.zero_seq_angle,
-            'negative_seq': self.negative_seq,
-            'negative_seq_angle': self.negative_seq_angle,
-            'positive_seq': self.positive_seq,
-            'positive_seq_angle': self.positive_seq_angle
-        }
-        return dict_seq
+
+cur_norm = np.array(cur_norm)
+                                     
+    def MeanNominalVoltageCalculation(self, 
+                                    SteadyStateNormal: np.ndarray) -> float:
+        for item in self.MeanNominalPhaseVoltages:
+            if SteadyStateNormal <= 1.2*item and SteadyStateNormal >= 0.8*item:
+                MeanNominalVoltage = item
+                break
+        return MeanNominalVoltage
+
+    
